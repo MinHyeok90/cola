8e443c084d1cbca230eabaaa82a6b689a50cf3d6
udambara <krivnon@gmail.com>
2016-08-19 21:40:49 +0900

    난 버튼 아이디를 만들고 음... 토스트메시지를 띄웠어? 유노?

--- app/src/main/java/com/example/android/cola/MainActivity.java
+++ app/src/main/java/com/example/android/cola/MainActivity.java
@@ -2,13 +2,21 @@ package com.example.android.cola;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = (Button)findViewById(R.id.hi);
+    }
+    void onClick(View v){
+        Toast.makeText(this,"hi",Toast.LENGTH_SHORT).show();
+
     }
 
 }
