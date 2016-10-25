0a47e6a259ac8ff9c1443f2a773c6dec4597dedc
udambara <krivnon@gmail.com>
2016-10-25 21:38:28 +0900

    gradle 수정

--- app/src/main/java/com/example/android/cola/SignUpActivity.java
+++ app/src/main/java/com/example/android/cola/SignUpActivity.java
@@ -15,7 +15,6 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.firebase.client.Firebase;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
